00bf0cf98fd6daabf7cc9fab952d2ef2819b5c05
theBozdemir <tarikbozdemir2602@gmail.com>
2025-05-04 10:15:34 -0400

    Pulling data from SQL Server

---
+++ cypress/e2e/RahulShetty/syllabus/dbTesting.cy.js
@@ -0,0 +1,12 @@
+context("DB Testing",()=>{
+    it("DB Testing",()=>{
+
+        cy.sqlServer("select * from Persons").then(function(result){
+
+            console.log(result[0][2][3])
+        })
+
+    })
+
+
+})
