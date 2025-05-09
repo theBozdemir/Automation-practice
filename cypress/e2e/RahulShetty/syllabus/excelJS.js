1070aa0d5ecfa342ee9f3c53653e54c4745d4177
theBozdemir <tarikbozdemir2602@gmail.com>
2025-05-09 09:11:11 -0400

    excel readAndWrite

--- cypress/e2e/RahulShetty/syllabus/excelJS.js
+++ cypress/e2e/RahulShetty/syllabus/excelJS.js
@@ -1,15 +1,32 @@
 const ExcelJs = require('exceljs');
 
-async function excelTest() {
-    const workbook = new ExcelJs.Workbook();
-    await workbook.xlsx.readFile('/Users/sevdis/downloads/exceltest.xlsx');
-    const worksheet = workbook.getWorksheet('Sheet1');
+    async function writeExcel(searcText,replaceText,filePath){
+        const workbook = new ExcelJs.Workbook();
+        await workbook.xlsx.readFile(filePath); 
+        const worksheet = workbook.getWorksheet('Sheet1');
+        const output=await readExcel(worksheet,searcText)
+        const cell = worksheet.getRow(output.row).getCell(output.column);
+        cell.value=replaceText
+        await workbook.xlsx.writeFile(filePath)
 
-    worksheet.eachRow((row, rowNumber) => {
-        row.eachCell((column, columnNumber) => {
-            console.log(`Row ${rowNumber}, Column ${columnNumber}: ${column.value}`);
+    }
+    async function readExcel(worksheet,searcText) {
+        let objectElement = {row:-1,column:-1}
+        worksheet.eachRow((row, rowNumber) => {
+        
+            row.eachCell((cell, columnNumber) => {
+                if(cell.value===searcText){
+                    objectElement.row=rowNumber
+                    objectElement.column=columnNumber
+    
+                }
+                
+            });
         });
-    });
-}
+        return objectElement;
 
-excelTest().catch(console.error);
+
+        
+    }
+    writeExcel("Iphone","Samsung",'/Users/sevdis/downloads/exceltest.xlsx')
+
